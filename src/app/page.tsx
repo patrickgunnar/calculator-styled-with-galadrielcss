739db465b51496ad7ecc43afa6cdb69141ebc4ab
Patrick Gunnar <patrickgunnar.me@gmail.com>
2025-01-04 12:40:34 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,107 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { MouseEvent, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [history, setHistory] = useState<string>("0");
+    const [result, setResult] = useState<string>("0");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    function handleClick(ev: MouseEvent<HTMLButtonElement>) {
+        const value = ev.currentTarget.innerText;
+
+        if (value === 'C') {
+            clearAll();
+        } else if (value === 'DEL') {
+            deleteLastChar();
+        } else if (value === '=') {
+            evaluateExpr();
+        } else {
+            appendChar(value);
+        }
+    }
+
+    function clearAll() {
+        setHistory("0");
+        setResult("0");
+    }
+
+    function deleteLastChar() {
+        setResult((prev) => prev.slice(0, -1));
+    }
+
+    function appendChar(val: string) {
+        setResult((prev) => {
+            if (prev === "0") return val;
+
+            return prev += val
+        });
+    }
+
+    function evaluateExpr() {
+        try {
+            const res = parseFloat(eval(result).toFixed(5)).toString();
+
+            setHistory(result + "=" + res);
+            setResult(res);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (_: unknown) {
+            setResult("Could not be evaluated.");
+        }
+    }
+
+    return (
+        <section className="@module:pageComponent::calculator">
+            <div className="@module:pageComponent::screenContainer">
+                <div className="@module:pageComponent::screen">
+                    <input
+                        className="@module:pageComponent::history"
+                        value={history}
+                        readOnly
+                    />
+                    <input
+                        className="@module:pageComponent::result"
+                        value={result}
+                        readOnly
+                    />
+                </div>
+            </div>
+            <div className="@module:pageComponent::btnContainer">
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::lightPink">
+                    C
+                </button>
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::lightPink">
+                    DEL
+                </button>
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::lightBlue">
+                    %
+                </button>
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::lightBlue">
+                    /
+                </button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">7</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">8</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">9</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::lightBlue">
+                    *
+                </button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">4</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">5</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">6</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::lightBlue">
+                    -
+                </button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">1</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">2</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">3</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::lightBlue">
+                    +
+                </button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">.</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn">0</button>
+                <button onClick={handleClick} className="@module:pageComponent::btn @module:pageComponent::equalsBtn">
+                    =
+                </button>
+            </div>
+        </section>
+    );
 }
